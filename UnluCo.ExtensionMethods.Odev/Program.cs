ea7205b9337da95bf69110a60b2d2b4bf868f722
Fatih Topcu <fthtpu@gmail.com>
2022-01-16 18:32:05 +0300

    first commit

---
+++ UnluCo.ExtensionMethods.Odev/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UnluCo.ExtensionMethods.Odev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
